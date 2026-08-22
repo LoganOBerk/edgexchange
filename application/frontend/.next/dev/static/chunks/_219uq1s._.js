359481e40bfb0cd2780d1f1246b5373e3d046aac
Logan Berk <146039828+LoganOBerk@users.noreply.github.com>
2026-08-22 17:18:56 -0400

    Stop tracking .next

--- application/frontend/.next/dev/static/chunks/_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/app_globals_0yg4wg8.css","static/chunks/node_modules_18xq7xd._.js","static/chunks/_1vpbdot._.js","static/chunks/node_modules_next_0rlaow_._.js","static/chunks/node_modules_recharts_es6_0-c6m14._.js","static/chunks/node_modules_19-kxi_._.js","static/chunks/_0ej7gcr._.js"],
-    source: "entry"
-});
